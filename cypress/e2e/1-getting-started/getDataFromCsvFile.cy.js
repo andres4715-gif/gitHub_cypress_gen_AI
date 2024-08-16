feb5f44e56e8e960aa60407cb3e1fb278e608ada
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-08-15 23:19:45 -0500

    new data added

--- cypress/e2e/1-getting-started/getDataFromCsvFile.cy.js
+++ cypress/e2e/1-getting-started/getDataFromCsvFile.cy.js
@@ -5,11 +5,11 @@ it('Get data from a .csv file and convert it to json file', () => {
   cy.readFile('cypress/files/Locations_import_template_1_records.csv', 'utf8').then((csvData) => {
     csvtojson().fromString(csvData).then((data) => {
       const fullData = data;
-      console.log(fullData);
+      console.log("--- Full data: ", fullData);
       const obtainedCity = data[0].city;
 
       if(obtainedCity === "Bogotá") {
-        console.log("✅");
+        console.log("✅", obtainedCity);
       } else {
         console.error("🔥");
       }
