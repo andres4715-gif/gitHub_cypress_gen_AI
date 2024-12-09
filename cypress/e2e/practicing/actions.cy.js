9bc17348e68163022a12a37d7dfed004731adbf6
Andres Rios <andres.rios@intelex.com>
2024-12-09 13:51:42 -0500

    new data

--- cypress/e2e/practicing/actions.cy.js
+++ cypress/e2e/practicing/actions.cy.js
@@ -4,36 +4,40 @@ context('Actions', () => {
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/actions');
   });
-  it('.type() - type into a DOM element', () => {
+
+  it('typing', () => {
     const email = 'andres798@gmail.com';
     const validEmail = 'andres4715@gmail.com';
-
-    // typing
+    cy.visit('https://example.cypress.io/commands/actions');
     cy.get('#email1').type(email);
     cy.get('#email1').should('have.value', email);
     cy.get('#email1').type('{selectall}{backspace}');
     cy.get('#email1').type(validEmail, { delay: 50 });
     cy.get('#email1').clear();
     cy.get('#email1').should('have.value', '');
+  });
 
-    // .as()
+  it('.as', () => {
     cy.get('#email1').as('email');
     cy.get('@email').type('✅✅✅✅✅');
     cy.get('@email').should('have.value', '✅✅✅✅✅');
+  });
 
-    // getting text() using a promise
+  it('getting text() using a promise', () => {
     cy.get('[for="couponCode1"]').then(data => {
       const finalData = data.text();
       expect(finalData).to.equal('Coupon Code');
     });
+  });
 
-    // getting text() using invoke();
+  it('getting text() using invoke()', () => {
     cy.get('.banner').invoke('text').then(data2 => {
       cy.log(data2);
       expect(data2).to.include('Examples of actions being performed on DOM');
     });
+  });
 
-    // .submit()
+  it('.submit', () => {
     cy.get('.action-form').as('submit');
     cy.get('@submit').find('[type="text"]').as('finalSubmit');
     cy.get('@finalSubmit').type('Rios ✅');
@@ -41,37 +45,69 @@ context('Actions', () => {
     cy.get('.action-form').as('submitAction');
     cy.get('@submitAction').submit();
     cy.get('.well > p').should('have.text', 'Your form has been submitted!');
+  });
 
-    // .click() using canvas:
+  it('.click() using canvas:', () => {
     cy.get('#action-canvas').as('canvas').click();
     cy.get('@canvas').click('topLeft');
     cy.get('@canvas').click('top');
+  });
 
-    // dblclick()
+  it(' dblclick()', () => {
     const addingData = 'checking double click';
     cy.get('.action-div').dblclick();
     cy.get('.action-input-hidden').clear().type(addingData);
     cy.wait(1000);
     cy.get('.action-input-hidden').should('have.value', addingData);
+  });
 
-    // check()
+  it('check()', () => {
     cy.get('.action-checkboxes [type="checkbox"]').first().check({ force: true });
     cy.get('.action-checkboxes [type="checkbox"]').should('be.checked');
     cy.get('.action-checkboxes [type="checkbox"]').first().uncheck({ force: true });
     cy.get('.action-checkboxes [type="checkbox"]').first().should('not.be.checked');
     cy.get('#optionsRadios1').check({ force: true });
+  });
 
-    // select()
+  it('select', () => {
     cy.get('.action-select').select('bananas');
+  });
 
-    // scrollIntoView()
+  it('scrollIntoView()', () => {
     cy.get('#scroll-horizontal button').as('scrollIntoViewButton').scrollIntoView();
     cy.get('@scrollIntoViewButton').click();
+  });
 
-    // invoke()
+  it('invoke()', () => {
     cy.visit('https://on.cypress.io/and');
     cy.get('.headerWrapper_tu51').first().invoke('text').then(data => {
       cy.log('🚀🚀🚀🚀🚀', data);
+    });
+  });
+
+  it('Connectors', () => {
+    cy.visit('https://example.cypress.io/commands/connectors');
+    let myData = [];
+    cy.get('.connectors-each-ul>li')
+      .each(function ($el, index, $list) {
+        const data = $el[0].innerText;
+        myData.push(data);
+
+        const newData = myData.filter(x => x.startsWith('L'));
+        console.log(newData);
+        if (newData.length > 0) {
+          console.log("✈️");
+        }
+      });
+  });
+
+  it('Connectors 2', () => {
+    cy.visit('https://example.cypress.io/commands/connectors');
+    let myArray = [];
+    cy.get('.connectors-each-ul>li').each(element => {
+      const newData = element[0].innerText;
+      myArray.push(newData);
+      console.log("Array of names:", myArray);
     });
   });
 });
